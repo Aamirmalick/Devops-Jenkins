550de7ba37479e7606eb86ce2a8506d95a991607
Aamir Malick <m.aamirmalick@gmail.com>
2023-05-20 21:43:20 +0530

    first commit

---
+++ src/main/java/com/example/AwsEc2SpringbootDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEc2SpringbootDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEc2SpringbootDeployApplication.class, args);
+	}
+
+}
